7285431fe2356548d16a230b91da322c6e92fc05
David Escarcega <davidaescarcega@gmail.com>
2025-10-28 20:00:33 -0700

    presentacion

--- presentacion_vista/src/main/java/presentacion/frames/FramePrincipal.java
+++ presentacion_vista/src/main/java/presentacion/frames/FramePrincipal.java
@@ -1,6 +1,26 @@
 package presentacion.frames;
 
+import presentacion.panels.ArmarEquipoPanel;
+import presentacion.panels.MenuPrincipalPanel;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class FramePrincipal extends JFrame {
+    private MenuPrincipalPanel menuPrincipalPanel;
+    private ArmarEquipoPanel  armarEquipoPanel;
+
+    public FramePrincipal() {
+        setTitle("Electronic store");
+        setSize(1200, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        menuPrincipalPanel = new MenuPrincipalPanel();
+        armarEquipoPanel = new ArmarEquipoPanel();
+
+
+        add(armarEquipoPanel);
+
+        setVisible(true);
+    }
 }
---
+++ presentacion_vista/src/main/java/presentacion/panels/MenuPrincipalPanel.java
@@ -0,0 +1,15 @@
+package presentacion.panels;
+
+import javax.swing.*;
+
+public class MenuPrincipalPanel extends JPanel {
+    private JButton boton1;
+
+    public MenuPrincipalPanel(){
+        boton1 = new JButton("Boton");
+
+        add(boton1);
+    }
+
+
+}
